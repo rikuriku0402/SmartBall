716eb50b1cd4991beef1976b329adb176140765c
riku0402 <04022005rikuriku@gmail.com>
2023-04-11 23:29:30 +0900

    ルーレット機能を更新した

--- Assets/SmartBall/Scripts/RouletteBall.cs
+++ Assets/SmartBall/Scripts/RouletteBall.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour,IRouletteeble
+public class RouletteBall : MonoBehaviour,IRouletteeble
 {
     private int _probability;
     
