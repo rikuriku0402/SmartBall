ec4bdd2cb2fc05d444048da592ec0759e62386a5
kanaya0730 <knysho@icloud.com>
2023-04-30 13:31:21 +0900

    棒の処理を修正しました。

--- Assets/SmartBall/Scripts/TensionRod.cs
+++ Assets/SmartBall/Scripts/TensionRod.cs
@@ -5,26 +5,37 @@ using UnityEngine;
 using UniRx.Triggers;
 using Cysharp.Threading.Tasks;
 using DG.Tweening;
+using UniRx;
 
 public class TensionRod : MonoBehaviour
 {
-    [SerializeField]
     private float _clickingTime;
 
     [SerializeField]
     private GameObject _ball;
 
     private Rigidbody _rd;
-
-    [SerializeField]
+    
     private Vector3 _currentPos;
 
     [SerializeField]
-    private float _power;
+    private float _power = 20;
+    
+    //y軸方向の移動範囲の最小値
+    [SerializeField]
+    private float _minY = -0.5f;
+
+    //y軸方向の移動範囲の最大値
+    [SerializeField]
+    private float _maxY = 10;
     private void Start()
     {
         _rd = _ball.GetComponent<Rigidbody>();
         _currentPos = gameObject.transform.position;
+        
+        this.UpdateAsObservable()
+            .Subscribe(_ => Limit())
+            .AddTo(this);
     }
 
     public void Drag()
@@ -40,9 +51,11 @@ public class TensionRod : MonoBehaviour
 
     public void Move()
     {
-        var posX = gameObject.transform.position.x;
-        var posZ = gameObject.transform.position.z;
-        transform.DOMove(new Vector3(posX, _clickingTime, posZ), 0.5f);
+        var pos = gameObject.transform.position;
+        
+        Limit();
+        transform.DOMove(new Vector3(pos.x, _clickingTime, pos.z), 0.5f);
+
     }
 
     public async void Shot()
@@ -51,9 +64,17 @@ public class TensionRod : MonoBehaviour
         transform.DOMove(_currentPos, 0.5f)
             .SetDelay(0.2f)
             .SetEase(Ease.OutBounce);
+        
         await UniTask.Delay(TimeSpan.FromSeconds(0.2f));
         _rd.AddForce(new Vector3(0,_clickingTime * -_power, 0), ForceMode.Impulse);
         _clickingTime = 0;
+    }
+
+    public void Limit()
+    {
+        //y軸方向の移動範囲制限
+        _clickingTime = Mathf.Clamp(_clickingTime, _minY, _maxY);
+
     }
 
 }
